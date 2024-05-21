285f8b24c26553c0a5910df63cf24a9b05580389
chandak8796 <chandakoli8796@gmail.com>
2024-05-21 14:54:34 +0530

    code update

--- src/main/java/com/easyshop/crud/controller/OrderController.java
+++ src/main/java/com/easyshop/crud/controller/OrderController.java
@@ -14,18 +14,9 @@ public class OrderController {
     @Autowired
     private OrderService service;
 
-//    @PostMapping
-//    public Order addOrder(@RequestBody Order order){
-//        return service.addOrder(order);
-//    }
-
     @GetMapping
     public List<Order> getOrders(){
         return service.getOrders();
     }
 
-//    @GetMapping("/{id}")
-//    public Order getOrderById(@PathVariable int id){
-//        return service.getOrderById(id);
-//    }
 }
--- src/main/java/com/easyshop/crud/service/OrderService.java
+++ src/main/java/com/easyshop/crud/service/OrderService.java
@@ -16,22 +16,6 @@ import java.util.Optional;
 
 @Service
 public class OrderService {
-
-//    @Autowired
-//    private OrderRepository repository;
-//
-//    public Order addOrder(Order order){
-//       return repository.save(order);
-//    }
-//
-//    public List<Order> getOrders(){
-//        return repository.findAll();
-//    }
-//
-//    public Order getOrderById(int id){
-//        return repository.findById(id)
-//                .orElseThrow(()->new IllegalArgumentException("Invalid id : "+id));
-//    }
     @Autowired
     private OrderRepository orderRepository;
     @Autowired
